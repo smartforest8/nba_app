e3a11f74ec8eda1da84a0ec25bf1fba59e1b982f
smartforest8 <masashi_komori@icloud.com>
2022-08-15 14:45:36 +0900

    debug errors

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def index():
 if __name__ == "__main__":
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(port=8000, debug=True)
+    app.run(port=8000, debug=False)
