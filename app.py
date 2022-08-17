57b20cb106b7a1db500d0a1138d0f34f7856ab21
smartforest8 <masashi_komori@icloud.com>
2022-08-17 15:16:53 +0900

    version0.3

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import os
-from flask import Flask, url_for, render_template, jsonify
-from functions.use_api import get_curry
+import json
+from flask import Flask, url_for, render_template, jsonify, request
+from functions.use_api import get_curry, get_playersdata, get_ShotData
 app = Flask(__name__)
 
 # ルーティング
-@app.route("/shotchart",methods=['GET'])
+@app.route("/shotchart",methods=['GET', 'POST'])
 def shortchart():
     json_curry = get_curry()
-    return render_template('shotchart.html', shotchart = json_curry)
+    nba_players = get_playersdata()
+    if request.method == 'GET':
+        return render_template('shotchart.html', shotchart = json_curry , nba_players = nba_players)
+    else:
+        print("json")
+        print(request.form['player_name'])
+        nba_players = get_playersdata()
+        player_id = json.loads(request.form['player_name'])['id']
+        player_name = json.loads(request.form['player_name'])['full_name']
+        json_curry = get_ShotData(player_id = player_id)
+        print(player_name)
+        return render_template('shotchart.html', shotchart = json_curry , nba_players = nba_players, player_name = player_name)
+
+
 
 @app.route("/", methods=['GET'])
 def index():
@@ -17,4 +31,4 @@ def index():
 if __name__ == "__main__":
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(port=8000, debug=False)
+    app.run(port=8000, debug=True)
--- functions/use_api.py
+++ functions/use_api.py
@@ -13,3 +13,22 @@ def get_curry():
 	json_text = response.get_json()
 	content = json.loads(json_text)
 	return content
+
+
+def get_ShotData(team_id=0,	player_id=201939, season_nullable='2021-22', context_measure_simple = 'FGA', season_type_all_star='Regular Season'):
+	response = shotchartdetail.ShotChartDetail(
+	    team_id=0,
+	    player_id=player_id,
+	    season_nullable='2021-22',
+	    context_measure_simple = 'FGA', #<-- Default is 'PTS' and will only return made shots, but we want all shot attempts
+	    season_type_all_star='Regular Season'
+	)
+
+	json_text = response.get_json()
+	content = json.loads(json_text)
+	return content
+
+def get_playersdata():
+	nba_players = players.get_players()
+
+	return nba_players
